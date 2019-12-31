90dc4c2952d8313f2b8dbf28799fc5fac62a90a3
李富荣 <lifurong@cloudwalk.cn>
2019-12-31 15:30:49 +0800

    update idole

--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -1,13 +1,18 @@
 <template>
-<v-container>
-    <v-card>
+<!-- <v-container> -->
+    <v-tabs dark background-color="teal" centered>
+    <v-tab>黑白糖果盒</v-tab>
+    <v-tab>思维肖申克</v-tab>
+    <v-tab>灵感之源</v-tab>
+  </v-tabs>
+    <!-- <v-card>
       <v-avatar>
         <v-img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2646714862,2989345588&fm=26&gp=0.jpg"></v-img>
       </v-avatar>
        <p class="font-weight-black">Black text.</p>
     </v-card>
-    <v-card></v-card>
-</v-container>
+    <v-card></v-card> -->
+<!-- </v-container> -->
 </template>
 
 <script>
--- src/views/Idoles.vue
+++ src/views/Idoles.vue
@@ -1,26 +1,24 @@
 <template>
 <v-container>
     <v-row>
-        <v-col cols='12' md='4' v-for='(idole,i) in idoles' :key="i">
-            <v-card shaped raised>
-                <v-card-title>{{idole.name}}
-                    <v-chip class='ml-1' dark small label color="teal">hh</v-chip>
-                    <v-chip class='ml-2' dark small label color="teal">hh</v-chip>
-                    <!-- <v-chip class='ml-2' dark small label color="teal">hh</v-chip> -->
-                    <v-spacer></v-spacer>
-                    <v-chip dark>hh</v-chip>
-                </v-card-title>
-                <v-card-subtitle>
-                    <span>{{idole.description}}</span>
+        <v-col cols='12' md='3' v-for='(idole,i) in idoles' :key="i">
+            <v-hover v-slot:default="{ hover }">
+                <v-card shaped :elevation="hover ? 12 : 2">
+                    <v-card-title>{{idole.name}}
+                        <v-spacer></v-spacer>
+                        <v-chip dark :color=idole.color>hh</v-chip>
+                    </v-card-title>
+                    <v-card-subtitle>
+                        <span>{{idole.description}}</span>
+                    </v-card-subtitle>
+                    <v-img aspect-ratio="0.9" :src=idole.avatar class='my-1'></v-img>
+                    <v-card-actions>
 
-                </v-card-subtitle>
-                <v-img aspect-ratio="0.9" :src=idole.avatar class='my-1'></v-img>
-                <v-card-actions>
-
-                    <v-spacer></v-spacer>
-                    <v-progress-circular></v-progress-circular>
-                </v-card-actions>
-            </v-card>
+                        <v-spacer></v-spacer>
+                        <v-progress-circular :color='idole.color' value="100"></v-progress-circular>
+                    </v-card-actions>
+                </v-card>
+            </v-hover>
         </v-col>
     </v-row>
 </v-container>
@@ -45,7 +43,37 @@ export default {
                 name: '怪盗KID',
                 avatar: require('@/assets/images/idoles/3.jpg'),
                 description: '月光下的魔术师',
-                color: 'white'
+                color: 'white darken-3'
+            },
+            {
+                name: '星马烈',
+                avatar: require('@/assets/images/idoles/4.jpg'),
+                description: '弯道王子',
+                color: 'red'
+            },
+            {
+                name: 'UnderTaker',
+                avatar: require('@/assets/images/idoles/5.jpg'),
+                description: '死亡谷的主人',
+                color: 'black'
+            },
+            {
+                name: 'Json Mraz',
+                avatar: require('@/assets/images/idoles/6.jpg'),
+                description: '231s',
+                color: 'red'
+            },
+            {
+                name: '拉斐内',
+                avatar: require('@/assets/images/idoles/7.jpg'),
+                description: 'Phoenix',
+                color: 'purple'
+            },
+            {
+                name: 'Pablo Aimar',
+                avatar: require('@/assets/images/idoles/8.jpg'),
+                description: '小丑',
+                color: 'blue lighten-3'
             }
         ]
     })
