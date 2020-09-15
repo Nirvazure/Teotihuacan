ff0076cce19fa333520f6e9dad22bea30c2219bc
Nirvazure <nirvazure@github.com>
2020-09-15 22:38:16 +0800

    update

--- src/views/Toys.vue
+++ src/views/Toys.vue
@@ -4,7 +4,7 @@
       <v-col cols="12" md="3">
         <v-list>
           <v-list-item-group>
-            <v-list-item v-for="(toy, index) in toys" :key="index">
+            <v-list-item v-for="(toy, index) in toys" :key="index" @click="tid=index">
               <v-list-item-avatar>
                 <v-img :src="toy.avatar"></v-img>
               </v-list-item-avatar>
@@ -17,7 +17,10 @@
         </v-list>
       </v-col>
       <v-col>
-        <v-card></v-card>
+        <v-card>
+          <v-card-title>{{tarToy.name}}</v-card-title>
+          <v-card-subtitle v-html="tarToy.description"></v-card-subtitle>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -27,8 +30,14 @@
 import toys from "@/api/toys";
 export default {
   data: () => ({
+    tid: 0,
     toys: toys,
   }),
+  computed: {
+    tarToy() {
+      return this.toys[this.tid];
+    },
+  },
 };
 </script>
 
