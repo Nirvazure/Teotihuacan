7626a4de6d0d0c1e4e4bf2b4234976f43ff7af73
李富荣 <lifurong1@cloudwalk.com>
2020-09-25 16:04:07 +0800

    update

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -69,18 +69,7 @@
           <v-window v-model="window" :show-arrows="true">
             <div class="text-center">
               <v-window-item v-for="(album, i) in albums" :key="i">
-<<<<<<< HEAD
                 <v-avatar size="300" class="mt-8">
-                  <v-img class="ma-4" :src="album.avatar" @mouseenter="isActive=!isActive"></v-img>
-                </v-avatar>
-                <h2 class="ma-2">{{ album.name }}</h2>
-                <v-chip class="mr-4" small label color="teal" dark>民谣</v-chip>
-                <v-chip class="mr-4" small label color="primary" dark>90后</v-chip>
-                <v-chip class="mr-4" small label color="black" dark>夜晚</v-chip>
-                <p class="my-4">西大街的夜晚</p>
-                <v-progress-linear :value="50" class="my-0" height="3" color="teal"></v-progress-linear>
-=======
-                <v-avatar size="300" class="mt-8 App-logo">
                   <v-img
                     class="ma-4"
                     :src="album.avatar"
@@ -102,7 +91,6 @@
                   height="3"
                   color="teal"
                 ></v-progress-linear>
->>>>>>> 0cb87513947d164756f26f43f4ebd12af549ce67
                 <v-list>
                   <v-list-item>
                     <v-list-item-avatar>
@@ -110,13 +98,9 @@
                     </v-list-item-avatar>
                     <v-list-item-content>
                       <v-list-item-title>The Walker</v-list-item-title>
-<<<<<<< HEAD
-                      <v-list-item-subtitle>Fitz & The Trantrums</v-list-item-subtitle>
-=======
                       <v-list-item-subtitle
                         >Fitz & The Trantrums</v-list-item-subtitle
                       >
->>>>>>> 0cb87513947d164756f26f43f4ebd12af549ce67
                     </v-list-item-content>
 
                     <v-spacer></v-spacer>
@@ -127,26 +111,18 @@
                       </v-btn>
                     </v-list-item-icon>
 
-<<<<<<< HEAD
-                    <v-list-item-icon :class="{ 'mx-5': $vuetify.breakpoint.mdAndUp }">
-=======
                     <v-list-item-icon
                       :class="{ 'mx-5': $vuetify.breakpoint.mdAndUp }"
                     >
->>>>>>> 0cb87513947d164756f26f43f4ebd12af549ce67
                       <v-btn icon>
                         <v-icon>mdi-pause</v-icon>
                       </v-btn>
                     </v-list-item-icon>
 
-<<<<<<< HEAD
-                    <v-list-item-icon class="ml-0" :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }">
-=======
                     <v-list-item-icon
                       class="ml-0"
                       :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }"
                     >
->>>>>>> 0cb87513947d164756f26f43f4ebd12af549ce67
                       <v-btn icon>
                         <v-icon>mdi-fast-forward</v-icon>
                       </v-btn>
