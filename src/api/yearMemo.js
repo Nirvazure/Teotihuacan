7c8772fb391a689ae2e67a4e304fe86cf0ec6e17
Nirvazure <nirvazure@github.com>
2020-09-29 23:12:33 +0800

    update imgs

--- src/api/yearMemo.js
+++ src/api/yearMemo.js
@@ -1,165 +1,22 @@
-export default [{
-    time: "2008",
-    description: "21321",
-    dead: [""],
-    archives: [""],
-    keywords: [
-      "打砸抢",
-      "手足口病",
-      "毒奶粉",
-      "汶川地震",
-      "奥运开幕",
-      "金融危机",
-    ],
-    movies: [],
-    books: [],
-    places: ["昆明1月2月"],
-    people: [],
-  },
+export default [
   {
-    time: "2016",
-    description: "和小朋友们玩的最多的一年，关系变得更好的一年",
-    months: [{
-        time: "JAN",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "FEB",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "MAR",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "APR",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "MAY",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "JUN",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "JUL",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技"],
-      },
-      {
-        time: "AUG",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "重庆"],
-      },
-      {
-        time: "SEP",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "OCT",
-        items: [{
-          time: ""
-        }],
-        keywords: ["考研"],
-      },
-      {
-        time: "NOV",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-      {
-        time: "DEC",
-        items: [{
-          time: ""
-        }],
-        keywords: ["毕业", "乔巴科技", "重庆", "考研"],
-      },
-    ],
-    archives: ["和小朋友关系越来越好"],
-    movies: [],
-    memos: [
-      require("@/assets/images/sketches/yqy (4).jpg"),
-      require("@/assets/images/sketches/yqy (5).jpg"),
-      require("@/assets/images/sketches/yqy (6).jpg"),
-    ],
-    books: [],
-    places: [],
-    people: ["关嘉伟", "赵静"],
-  },
-  {
-    time: "2017",
-    description: "支付宝全年消费8400",
-    dead: ["余光中"],
-    archives: ["和小朋友关系越来越好"],
-    movies: [],
-    books: [],
-    places: [],
-    memos: [
-      require("@/assets/images/sketches/yqy (1).jpg"),
-      require("@/assets/images/sketches/yqy (2).jpg"),
-      require("@/assets/images/sketches/yqy (3).jpg"),
-
-
-    ],
-    people: ["关嘉伟", "赵静"],
-  },
-  {
-    time: "2018",
+    time: "2020",
     description: "",
-    dead: [""],
-    archives: [""],
     memos: [
-      require("@/assets/images/sketches/yqy (11).jpg"),
-      require("@/assets/images/sketches/yqy (12).jpg"),
-      require("@/assets/images/sketches/yqy (13).jpg"),
+      require("@/assets/images/memos/2020 (1).jpg"),
+      require("@/assets/images/memos/2020 (2).jpg"),
+      require("@/assets/images/memos/2020 (3).jpg"),
     ],
-    keywords: ["LOVE", "SKETCH", "形象改变"],
-    items: ["DELL G3"],
-    movies: [],
-    books: [],
-    places: [],
-    people: ["郝慧", "王勤"],
+    keywords: ["疫情", "答辩", "爷爷"],
   },
   {
     time: "2019",
-    description: "",
-    dead: [],
-    archives: ["去了上海"],
     keywords: ["VLOG", "秋招"],
     movies: [],
     memos: [
-      require("@/assets/images/sketches/yqy (14).jpg"),
-      require("@/assets/images/sketches/yqy (15).jpg"),
-      require("@/assets/images/sketches/yqy (16).jpg"),
+      require("@/assets/images/memos/2019 (4).jpg"),
+      require("@/assets/images/memos/2019 (2).jpg"),
+      require("@/assets/images/memos/2019 (3).jpg"),
     ],
     items: [
       "Adineo卫衣",
@@ -180,14 +37,40 @@ export default [{
     people: ["云从小伙伴", "王浩文", "徐亚茹"],
   },
   {
-    time: "2020",
+    time: "2018",
     description: "",
-    dead: [],
-    archives: [],
-    keywords: ["疫情", "答辩"],
+    memos: [
+      require("@/assets/images/memos/2018 (1).jpg"),
+      require("@/assets/images/memos/2018 (2).jpg"),
+      require("@/assets/images/memos/2018 (3).jpg"),
+    ],
+    keywords: ["LOVE", "SKETCH", "形象改变"],
+    items: ["DELL G3"],
+    people: ["郝慧", "王勤"],
+  },
+  {
+    time: "2017",
+    description: "支付宝全年消费8400",
+    dead: ["余光中"],
+    archives: ["和小朋友关系越来越好"],
     movies: [],
     books: [],
     places: [],
-    people: ["王帅"],
+    memos: [
+      require("@/assets/images/sketches/yqy (1).jpg"),
+      require("@/assets/images/sketches/yqy (2).jpg"),
+      require("@/assets/images/sketches/yqy (3).jpg"),
+    ],
+    people: ["关嘉伟", "赵静"],
+  },
+  {
+    time: "2016",
+    description: "和小朋友们玩的最多的一年，关系变得更好的一年",
+    memos: [
+      require("@/assets/images/sketches/yqy (4).jpg"),
+      require("@/assets/images/sketches/yqy (5).jpg"),
+      require("@/assets/images/sketches/yqy (6).jpg"),
+    ],
+    people: ["关嘉伟", "赵静"],
   },
 ];
