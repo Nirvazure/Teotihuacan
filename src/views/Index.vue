4e0de3a7ea686ef9104252550d88fe307ed84251
李富荣 <lifurong1@cloudwalk.com>
2020-09-30 14:32:31 +0800

    update index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,30 @@
 <template>
-  <v-parallax class="text-center pa-0" :height="exploreHeight" dark :src="bgImg">
+  <v-parallax
+    class="text-center pa-0"
+    :height="exploreHeight"
+    dark
+    :src="bgImg"
+  >
     <v-row>
       <v-col
         align-self="center"
         class="animated zoomIn"
-        v-bind:class="{ activeClass : isActive}"
+        v-bind:class="{ activeClass: isActive }"
         @mouseenter="addActive"
       >
         <v-avatar size="150" class="avatar">
           <v-img :src="avatar"></v-img>
         </v-avatar>
-        <h3 class="display-2 font-weight-bold">Nirvazure</h3>
-        <v-btn class="mt-3" dark color="teal" @click="$router.push({ path: `/home` })">enter my mind</v-btn>
+        <h3 class="display-2 font-Forte">
+          <vue-typer class="fontForm" text="Nirvazure"></vue-typer>
+        </h3>
+        <v-btn
+          class="mt-3"
+          dark
+          color="teal"
+          @click="$router.push({ path: `/home` })"
+          >enter my mind</v-btn
+        >
       </v-col>
     </v-row>
   </v-parallax>
@@ -19,8 +32,9 @@
 
 <script>
 import countTo from "vue-count-to";
+import { VueTyper } from "vue-typer";
 export default {
-  components: { countTo },
+  components: { countTo, VueTyper },
   data: () => ({
     isActive: true,
     activeClass: "animated bounce infinate",
@@ -42,4 +56,21 @@ export default {
 </script>
 
 <style>
+.vue-typer {
+  font-family: Copperplate, "Forte", fantasy;
+  color: #fff;
+}
+
+.vue-typer .custom.char.typed {
+  color: #fff;
+}
+.vue-typer .custom.char.selected {
+  color: #fff;
+  background-color: transparent;
+  text-decoration: line-through;
+}
+
+.vue-typer .custom.caret {
+  display: none;
+}
 </style>
