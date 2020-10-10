0e0fd03aa559841ff2212391f707f11c419d2e60
李富荣 <lifurong1@cloudwalk.com>
2020-10-10 19:16:20 +0800

    update toy

--- src/api/toys.js
+++ src/api/toys.js
@@ -1,113 +1,83 @@
-export default [
-  {
+export default [{
     name: "斗破杀",
     description: "",
     avatar: "https://f11.baidu.com/it/u=3043060502,3321405125&fm=72",
-    type: "",
-    title: ""
   },
   {
     name: "Mindoney",
     description: "产品核心功能受众：大学生、种草爱好者扩展：和小红书、毒、YOHO定位：记账软件+心愿单（添加心愿，价格随着更改的价格而变化）扩展：心愿库社交（扩展功能）",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577945292&di=8c7ff11401ee7d5bd6df1c629e3f8114&imgtype=jpg&er=1&src=http%3A%2F%2Fimg01.taopic.com%2F171106%2F318752-1G10613522091.jpg"
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577945292&di=8c7ff11401ee7d5bd6df1c629e3f8114&imgtype=jpg&er=1&src=http%3A%2F%2Fimg01.taopic.com%2F171106%2F318752-1G10613522091.jpg"
   },
   {
     name: "DoraPocket",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577350620440&di=8c3a87301b12e9e32f1dd9a965eafc24&imgtype=0&src=http%3A%2F%2Fgss0.baidu.com%2F94o3dSag_xI4khGko9WTAnF6hhy%2Fzhidao%2Fpic%2Fitem%2Fb21bb051f8198618a63dac154ced2e738bd4e62d.jpg"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577350620440&di=8c3a87301b12e9e32f1dd9a965eafc24&imgtype=0&src=http%3A%2F%2Fgss0.baidu.com%2F94o3dSag_xI4khGko9WTAnF6hhy%2Fzhidao%2Fpic%2Fitem%2Fb21bb051f8198618a63dac154ced2e738bd4e62d.jpg"
   },
   {
     name: "YQYMONs",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://img.zcool.cn/community/01eca85ca73beea80121416862e3e9.jpg@260w_195h_1c_1e_1o_100sh.jpg"
+
+    avatar: "https://img.zcool.cn/community/01eca85ca73beea80121416862e3e9.jpg@260w_195h_1c_1e_1o_100sh.jpg"
   },
   {
     name: "ArtMind",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "鬼谷夜话",
     description: "2313",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "PlaneBlock",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "乌托邦杂货店",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "ComiChat",
     description: "系统每天给用户随机分配一幕，编写每章要有字数限制，每个故事最多20幕",
-    type: "",
     title: "创造故事的连环画应用，来源于2-410的宿舍白板",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "中国足球漫画",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "好友FM",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "旋律锁屏",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "iWeek",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   },
   {
     name: "王者之魂",
     description: "",
-    type: "",
-    title: "",
-    avatar:
-      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
+
+    avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577353253604&di=8e806bbed6835313eff3af208a052458&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F240171776485a7c35a09c7bd5fff4450884e76efc3a2-dyfvD6_fw658"
   }
 ];
--- src/views/Toys.vue
+++ src/views/Toys.vue
@@ -1,48 +1,42 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="3">
-        <v-list>
-          <v-list-item-group>
-            <v-list-item
-              v-for="(toy, index) in toys"
-              :key="index"
-              @click="tid = index"
+      <v-col cols="12" md="2" v-for="(item, i) in toys" :key="i">
+        <v-card height="400" class="pt-1 text-center" :img="item.bgImg">
+          <v-card-text class="mt-2">
+            <v-avatar size="100">
+              <v-img :src="item.avatar"></v-img>
+            </v-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold title">{{
+                item.name
+              }}</v-list-item-title>
+              <v-list-item-subtitle>{{
+                item.description
+              }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-chip
+              v-for="(label, i) in item.labels"
+              :key="i"
+              class="mt-3 mx-1"
+              label
+              :color="label.color"
+              dark
+              small
+              >{{ label.text }}</v-chip
             >
-              <v-list-item-avatar>
-                <v-img :src="toy.avatar"></v-img>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title v-html="toy.name"></v-list-item-title>
-                <v-list-item-subtitle
-                  v-html="toy.description"
-                ></v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-card>
-          <v-container>
-            <v-row>
-              <v-col class="ml-5">
-                <v-progress-circular width="20" size="200" :value="70" color="blue-grey"></v-progress-circular>
-              </v-col>
-              <v-col class="mr-5">
-                <v-progress-linear class="my-5" height="20" color="light-blue" value="10" striped></v-progress-linear>
-                <v-progress-linear
-                  class="my-5"
-                  height="20"
-                  color="light-green darken-4"
-                  value="20"
-                  striped
-                ></v-progress-linear>
-                <v-progress-linear class="my-5" height="20" value="45" striped color="lime"></v-progress-linear>
-                <v-progress-linear class="my-5" height="20" value="60" striped color="deep-orange"></v-progress-linear>
-              </v-col>
-            </v-row>
-          </v-container>
+          </v-card-text>
+          <v-card-text>
+            <v-avatar size="30">
+              <v-img :src="avatar"></v-img>
+            </v-avatar>
+            <v-avatar size="30">
+              <v-img :src="avatar"></v-img>
+            </v-avatar>
+            <v-avatar size="30">
+              <v-img :src="avatar"></v-img>
+            </v-avatar>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -53,14 +47,8 @@
 import toys from "@/api/toys";
 export default {
   data: () => ({
-    tid: 0,
     toys: toys,
   }),
-  computed: {
-    tarToy() {
-      return this.toys[this.tid];
-    },
-  },
 };
 </script>
 
