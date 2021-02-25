e3c05993c14f19f08889158a60b6b19f45b44a9d
李富荣 <lifurong1@cloudwalk.com>
2021-02-25 18:40:07 +0800

    update clock

--- src/views/Love.vue
+++ src/views/Love.vue
@@ -1,12 +1,29 @@
 <template>
   <v-container>
-    <h2>开发中</h2>
+    <h2>{{ title }}</h2>
+    <v-btn @click="start">Start</v-btn>
+    <v-btn @click="stop">Stop</v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
+  data: () => ({
+    title: "222",
+    timer: null,
+  }),
+  methods: {
+    start() {
+      this.timer = setInterval(() => {
+        const list = ["mid", "top", "jug", "bot", "sup"];
+        let index = Math.floor(Math.random() * list.length);
+        this.title = list[index];
+      }, 50);
+    },
+    stop() {
+      clearInterval(this.timer);
+    },
+  },
 };
 </script>
 
